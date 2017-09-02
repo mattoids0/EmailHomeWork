0b53672ad913e493392459c5993cfe71600b49ee
Massimo Lauria <lauria.massimo@gmail.com>
2017-09-02 16:24:59 +0200

    Add initial files

---
+++ README.md
@@ -0,0 +1,17 @@
+# emailhw
+
+## Summary
+
+__SUMMARY__
+
+## Contributing
+
+Yes, please do! See [CONTRIBUTING][] for guidelines.
+
+## License
+
+See [COPYING][]. Copyright (c) 2017 Massimo Lauria.
+
+
+[CONTRIBUTING]: ./CONTRIBUTING.md
+[COPYING]: ./COPYING
---
+++ emailhw/prjdata.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+__project_name__        = 'emailhw'
+__project_description__ = 'Software to manage homework via email'
+
+__author__              = 'Massimo Lauria'
+__author_email__        = 'lauria.massimo@gmail.com'
+__aurhor_string__       = __author__ + ' <' + __author_email__ + '>'
+
+__version_short__       = '0.0.0.1'
+__version_long__        = '0.0.0.1'
+__version__             = __version_long__
+
+__copyright__           = '2017 '+ __author__ + ' <' + __author_email__ + '>'
+__license__             = 'GPL-3'
+__url__                 = ''
+__repo_url__            = ''
