f18a0cf752f5c05f81a0a128d640bc9eb1c14438
Massimo Lauria <lauria.massimo@gmail.com>
2017-09-09 10:41:50 +0200

    Another bit of structure to the project.

---
+++ tests/test_compile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+import unittest
+
+class TestCompile(unittest.TestCase):
+    """Test the compiler for the commands
+    """
+
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
